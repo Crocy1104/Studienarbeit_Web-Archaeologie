758f7852f103bc6aaa12b6db0384910d881cfcef
Crocy1104 <marcocrocoll@gmx.de>
2017-11-22 21:06:30 +0100

    Initial commit

---
+++ request.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def request(url, timestamp):
+    payload = {'url': url, 'timestamp': timestamp}
+    r = requests.get('https://archive.org/wayback/available', params=payload)
+    print(r.text)
+
+
+url = input("URL: ")
+timestamp = input("Timestamp: ")
+
+request(url, timestamp);
+
+
+
