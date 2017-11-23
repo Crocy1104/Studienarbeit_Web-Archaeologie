204fc7f11a65387f589ada6595bb48d49c8ef3bc
Crocy1104 <marcocrocoll@gmx.de>
2017-11-23 12:58:35 +0100

    request_auto working --> type in URL and get the archive of the last 23 years in steps of 6 months

--- request_auto.py
+++ request_auto.py
@@ -2,9 +2,18 @@ import requests
 import datetime
 from dateutil.relativedelta import relativedelta
 
+
+def request(url, timestamp):
+    payload = {'url': url, 'timestamp': timestamp}
+    r = requests.get('https://archive.org/wayback/available', params=payload)
+    print(r.text)
+
+
+url = input("URL: ")
+
 date = datetime.date(1996, 1, 1)
 
 for i in range(45):
     datestr = str(date)
-    print(datestr.replace('-', ''))
+    print(request(url, datestr.replace('-', '')))
     date += relativedelta(months=6)
